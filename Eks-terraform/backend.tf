f7676deed6e7bf9afdc4906f9e77e2f01178868f
ajayrajsingh31 <121920229+ajayrajsingh31@users.noreply.github.com>
2024-09-04 15:54:50 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "s3-ajay-bucket" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Jenkins/env2/terraform.tfstate"
     region = "ap-south-1"
   }
 }
